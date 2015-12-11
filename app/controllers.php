34cd60f2b1097f4eca1ff2f8539e0f8bf59070e9
Will Oller <will@willoller.com>
2015-12-10 17:29:04 -0800

    tweaks

--- app/controllers.php
+++ app/controllers.php
@@ -1,31 +1,17 @@
 <?php
 
-use Symfony\Component\Templating\PhpEngine;
-use Symfony\Component\Templating\TemplateNameParser;
-use Symfony\Component\Templating\Loader\FilesystemLoader;
-
-$app = new Silex\Application();
-
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-    'monolog.logfile' => __DIR__.'/development.log',
-));
-
-$app['monolog']->addDebug('Testing the Monolog logging.');
-
-$app->get('/', function() use($app) {
-
-    return require __DIR__.'/../app/views/welcome.php';
-
+$app->get('/', function() use($app, $templating)
+{
+    return $templating->render('welcome.php', []);
 });
 
-$app->post('/print', function() use ($app) {
-
+$app->post('/print', function() use ($app)
+{
     $pages = $_POST['pages'];
 
     $doc = new Models\Document();
 
-    foreach ($pages as $p)
-    {
+    foreach ($pages as $p) {
         $doc->addCard($p);
     }
 
@@ -34,7 +20,5 @@ $app->post('/print', function() use ($app) {
     $app->setContentDisposition(ResponseHeaderBag::DISPOSITION_INLINE, 'page.pdf');
 
     return;
-
 });
 
-$app->run();
